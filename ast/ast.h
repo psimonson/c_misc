cd5e1fef4eaeb2865cd467f563f6148cd31b96b5
Philip Simonson <drunkenmaster1988@gmail.com>
2018-05-26 09:54:48 -0400

    Added simple AST (Abstract Syntax Tree) project test.

---
+++ ast/ast.h
@@ -0,0 +1,27 @@
+#ifndef PRS_AST_H
+#define PRS_AST_H
+
+#define MAXOPLEN 64
+
+typedef struct Exp {
+	enum { integer_exp, binary_exp, variable_exp } tag;
+	union {
+		int integer;
+		struct {
+			char op[MAXOPLEN];
+			struct Exp *left;
+			struct Exp *right;
+		} binary;
+		struct {
+			char op[MAXOPLEN];
+			struct Exp *left;
+			struct Exp *right;
+		} variable;
+	} op;
+} ast;
+
+ast *make_binaryExp(char *, ast *, ast *);
+ast *make_integerExp(int);
+void free_ast(ast *p);
+
+#endif
