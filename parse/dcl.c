190a78a49857eca3c2c67966514fe5edb1ebd076
Philip Simonson <drunkenmaster1988@gmail.com>
2018-06-14 18:27:59 -0400

    Added my simple C parser

---
+++ parse/dcl.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "parser.h"
+
+extern int tokentype;
+
+int main() /* convert declaration to words */
+{
+	while (gettoken() != EOF) {
+		strcpy(datatype, token); /* 1st token is data type */
+		out[0] = '\0';
+		dcl();	/* parse rest of line */
+		if (tokentype != '\n')
+			printf("syntax error\n");
+		printf("%s: %s %s\n", name, out, datatype);
+	}
+	return 0;
+}
