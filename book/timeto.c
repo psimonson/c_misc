681fadd0b17a04bbe4acf89643cfc1c108cb6b44
Philip Ruben Simonson <drunkenmaster1988@gmail.com>
2018-07-02 19:41:21 -0400

    Added a simple timeto program (how many days until a date)

---
+++ book/timeto.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+	int gettime(time_t*, time_t*);
+
+	printf("Days to go from current date to now\nis %d days.\n",
+		gettime(NULL, NULL));
+	return 0;
+}
+
+time_t setdate(struct tm *tmp, int day, int month, int year)
+{
+	tmp->tm_year = year-1900;
+	tmp->tm_mon = month-1;
+	tmp->tm_mday = day;
+	return mktime(tmp);
+}
+
+int gettime(time_t *beg, time_t *end)
+{
+	struct tm *tmp;
+	time_t now, then;
+
+	/* get local time */
+	now = time(NULL);
+	tmp = localtime(&now);
+	if (!tmp) {
+		beg = NULL;
+		end = NULL;
+		return 0;
+	}
+
+	/* make time from above */
+	then = setdate(tmp, 26, 10, 2018);
+	if (beg != NULL)
+		*beg = now;
+	if (end != NULL)
+		*end = then;
+
+	return (int)(difftime(then, now) / (60*60*24));
+}
+
